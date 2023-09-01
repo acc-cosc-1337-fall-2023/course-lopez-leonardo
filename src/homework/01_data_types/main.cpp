5076bfc60ce0fb8326920ec5efdd04f3f0dce23a
lopez-leonardo <143145194+lopez-leonardo@users.noreply.github.com>
2023-09-01 21:21:21 +0000

    hw1: add and multiply numbers function

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,25 @@
 ﻿//write include statements
-
-
+#include "data_types.h"
+#include<iostream>
 //write namespace using statement for cout
-
+using std::cin; using std::cout;
 
 int main()
 {
-	
+	auto num = 0;
+
+	cout << "Enter a number: ";
+	cin>>num;
+
+	auto result = 0;
+	result = multiply_numbers(num);
+
+	cout<<"Result is: " <<result<<"\n";
+
+	auto num1 = 4;
+	result = multiply_numbers(num1);
+
+	cout<<"Result is: "<<result<<"\n";
 
 	return 0;
 }
