48dbae45d62ab7f6702cf4eddecf14521adca87d
lopez-leonardo <143145194+lopez-leonardo@users.noreply.github.com>
2023-09-21 00:23:12 +0000

    modified:   src/homework/04_repetition/main.cpp 	modified:   src/homework/04_repetition/repetition.cpp 	modified:   src/homework/04_repetition/repetition.h 	modified:   test/homework/04_repetition/04_iteration_tests.cpp

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,41 @@
 //write include statements
-
+#include "repetition.h"
+#include <iostream>
+using std::cout;using std::cin;
 //write using statements
 
 int main() 
 {
+	int answer;
+	int num_in_question;
+	int num_in_question1;
+	int num_in_question2;
+	int response;
+
+	do{
+		cout<<"1 - Factorial \n2 - Greatest Common Divisor \n3 - Exit \n";
+		cin>>answer;
+
+		if(answer == 1){
+			cout<<"Enter a number to find out it's factorial: ";
+			cin>>num_in_question;
+			cout<<factorial(num_in_question)<<"\n";
+		}
+		if(answer == 2){
+			cout<<"Enter two numbers to find the greatest commmon factor: ";
+			cin>>num_in_question1;
+			cin>>num_in_question2;
+			cout<<gcd(num_in_question1,num_in_question2)<<"\n";
+		}
+		if(answer == 3){
+			cout<<"Are you sure you want to exit?\nType 1 for yes and type 2 for no: ";
+			cin>>response;
+			if(response == 2){
+				answer = 0;
+			}
+		}
+
+	}while(answer != 3);
+
 	return 0;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,27 @@
 //add include statements
+#include "repetition.h"
 
 //add function(s) code here
+
+int factorial(int num){
+    int sum = num;
+    
+    while(num > 1){
+        sum = sum * (num - 1);
+        num -= 1;
+    }
+
+    return sum;
+}
+
+int gcd(int num1, int num2){
+    int biggest_factor = 0;
+
+    for(int i = 1; i <= num1 && i <= num2; i++){
+        if(num1 % i == 0 && num2 % i == 0){
+            biggest_factor = i;
+        }
+    }
+
+    return biggest_factor;
+}
